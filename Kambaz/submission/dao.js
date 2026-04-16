d01b848e4fe3854570dbae539234443a33cc18ea
Alexandria Wright <wright.alex@northeastern.edu>
2026-04-16 14:10:53 -0400

    fix

--- Kambaz/submission/dao.js
+++ Kambaz/submission/dao.js
@@ -2,7 +2,7 @@ import { v4 as uuidv4 } from "uuid";
 import model from "./model.js";
 export default function SubmissionDao() {
   function findAllSubmissionsByUser(quizId, userId) {
-    return model.find({ quizId: quizId, userId: userId});
+    return model.find({ quizId: quizId, userId: userId}) || [];
   }
   function createSubmission(submission, quizId, userId) {
     const newSubmission = { ...submission, _id: uuidv4(), quizId: quizId, userId: userId };
